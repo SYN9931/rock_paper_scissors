b4d6ae98061db0a60d99251d9a2403da719bd486
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-09-21 06:43:43 +0800

    add user prompt and assign to userSelection

--- javascript.js
+++ javascript.js
@@ -1,19 +1,19 @@
 //rock-paper-scissors
 let getComputerChoice = Math.floor((Math.random()*3)+1);
+let userSelection = prompt ();
+console.log ("user: " + userSelection);
+
 
 if (getComputerChoice == 1) {
     const computerSelection = "rock";
-    console.log (computerSelection);
+    console.log ("computer: " + computerSelection);
 }
 else if (getComputerChoice== 2) {
     const computerSelection = "paper";
-    console.log (computerSelection);
+    console.log ("computer: " + computerSelection);
 }
 else {
     const computerSelection = "scissors";
-    console.log (computerSelection);
+    console.log ("computer: " + computerSelection);
 }
-
-
-
 
