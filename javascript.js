5253bfb23af8165a70f60edba44f306e7e24e89d
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-09-21 05:42:23 +0800

    create a random number generator between 1,2 and 3
    assign each number with rock,paper,scissors accordingly

---
+++ javascript.js
@@ -0,0 +1,14 @@
+//rock-paper-scissors
+let x = Math.floor((Math.random()*3)+1);
+//alert(x);
+
+if (x == 1) {
+    alert ("rock");
+}
+else if (x== 2) {
+    alert ("paper");
+}
+else {
+    alert ("scissors");
+}
+
