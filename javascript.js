698e1e5a876a7abd72b93443b73eb786219ca2b7
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-09-22 04:09:35 +0800

    modified the if else statement
    to compare user selected value and computer selected value.

--- javascript.js
+++ javascript.js
@@ -5,15 +5,40 @@ userSelection = userInput.toLowerCase();
 console.log ("user: " + userSelection);
 
 if (getComputerChoice == 1) {
-    const computerSelection = "rock";
-    console.log ("computer: " + computerSelection);
-}
-else if (getComputerChoice== 2) {
     const computerSelection = "paper";
     console.log ("computer: " + computerSelection);
+
+    if (userSelection == "paper") {
+        console.log("both of you picked paper. It's a draw!");
+    } else if (userSelection == "rock") {
+        console.log(userSelection + " beats " + computerSelection + ". You won!" );
+    } else {
+        console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+    }
 }
-else {
+else if (getComputerChoice== 2) {
     const computerSelection = "scissors";
     console.log ("computer: " + computerSelection);
+
+    if (userSelection == "scissors") {
+        console.log("both of you picked scissors. It's a draw!");
+    } else if (userSelection == "rock") {
+        console.log(userSelection + " beats " + computerSelection + ". You won!" );
+    } else {
+        console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+    }
+}
+else {
+    const computerSelection = "rock";
+    console.log ("computer: " + computerSelection);
+
+
+    if (userSelection == "rock") {
+        console.log("both of you picked rock. It's a draw!");
+    } else if (userSelection == "paper") {
+        console.log(userSelection + " beats " + computerSelection + ". You won!" );
+    } else {
+        console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+    }
 }
 
