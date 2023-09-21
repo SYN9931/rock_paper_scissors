c71643f777986177784a17651596a9467bdc4ac4
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-09-22 04:19:46 +0800

    added a filter to test for correct user input

--- javascript.js
+++ javascript.js
@@ -4,41 +4,47 @@ let userInput = prompt();
 userSelection = userInput.toLowerCase();
 console.log ("user: " + userSelection);
 
-if (getComputerChoice == 1) {
-    const computerSelection = "paper";
-    console.log ("computer: " + computerSelection);
+if (userSelection == "rock" || userSelection == "paper" || userSelection == "scissors") {
+    if (getComputerChoice == 1) {
+        const computerSelection = "paper";
+        console.log ("computer: " + computerSelection);
 
-    if (userSelection == "paper") {
-        console.log("both of you picked paper. It's a draw!");
-    } else if (userSelection == "rock") {
-        console.log(userSelection + " beats " + computerSelection + ". You won!" );
-    } else {
-        console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+        if (userSelection == "paper") {
+            console.log("both of you picked paper. It's a draw!");
+        } else if (userSelection == "rock") {
+            console.log(userSelection + " beats " + computerSelection + ". You won!" );
+        } else {
+            console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+        }
     }
-}
-else if (getComputerChoice== 2) {
-    const computerSelection = "scissors";
-    console.log ("computer: " + computerSelection);
+    else if (getComputerChoice== 2) {
+        const computerSelection = "scissors";
+        console.log ("computer: " + computerSelection);
 
-    if (userSelection == "scissors") {
-        console.log("both of you picked scissors. It's a draw!");
-    } else if (userSelection == "rock") {
-        console.log(userSelection + " beats " + computerSelection + ". You won!" );
-    } else {
-        console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+        if (userSelection == "scissors") {
+            console.log("both of you picked scissors. It's a draw!");
+        } else if (userSelection == "rock") {
+            console.log(userSelection + " beats " + computerSelection + ". You won!" );
+        } else {
+            console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+        }
     }
-}
-else {
-    const computerSelection = "rock";
-    console.log ("computer: " + computerSelection);
+    else {
+        const computerSelection = "rock";
+        console.log ("computer: " + computerSelection);
 
 
-    if (userSelection == "rock") {
-        console.log("both of you picked rock. It's a draw!");
-    } else if (userSelection == "paper") {
-        console.log(userSelection + " beats " + computerSelection + ". You won!" );
-    } else {
-        console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+        if (userSelection == "rock") {
+            console.log("both of you picked rock. It's a draw!");
+        } else if (userSelection == "paper") {
+            console.log(userSelection + " beats " + computerSelection + ". You won!" );
+        } else {
+            console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+        }
     }
+
+} else {
+    console.log("invalid input. Please try again.");
 }
+
 
