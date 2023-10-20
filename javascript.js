f6738dbc1b0ec3d00beea48e36d0f6048198e008
SYN9931 <132620624+SYN9931@users.noreply.github.com>
2023-10-21 06:07:16 +0800

    corrected the rps logic -removed the if else statement that causes paper to beat scissors -removed unnecessary lines

--- javascript.js
+++ javascript.js
@@ -9,14 +9,14 @@ let lose=0;
 
 let play = function (val) {
 if ( win <5 && lose<5){
-  //  console.log ("------Round " + i + "------");
-    let getComputerChoice = Math.floor(Math.random()*3)+1;
-    let userInput = val;
-    userSelection = userInput.toLowerCase();
+  //get random number between 1,2 or 3 for computer
+    let getComputerChoice = (Math.floor(Math.random()*3))+1;
+    console.log("randomizer value: " + getComputerChoice);
+    //get value from the button that player clicked
+    let userSelection = val;
     console.log ("user: " + userSelection);
     
 
-    if (userSelection == "rock" || userSelection == "paper" || userSelection == "scissors") {
         if (getComputerChoice === 1) {
             const computerSelection = "paper";
             console.log ("computer: " + computerSelection);
@@ -25,20 +25,16 @@ if ( win <5 && lose<5){
                 console.log("both of you picked paper. It's a draw!");
                 draw = draw+1;
             } else if (userSelection == "rock") {
-                console.log(userSelection + " beats " + computerSelection + ". You won!" );
-                win = win + 1;
-            } else {
-                console.log (userSelection + " beaten by " + computerSelection + ". You lose!" );
+                console.log(userSelection + " beaten by " + computerSelection + ". You lose!" );
                 lose = lose + 1;
+            } else {
+                console.log (userSelection + " beats " + computerSelection + ". You won!" );
+                win = win + 1;
             }
         }
         else if (getComputerChoice=== 2) {
             const computerSelection = "scissors";
             console.log ("computer: " + computerSelection);
-            //let div = document.createElement('div');
-            //div.id = 'computerSelection';
-            //div.innerHTML = '<h3>The Computer chose: Scissors</h3>';
-            //document.body.appendChild(div);
 
             if (userSelection == "scissors") {
                 console.log("both of you picked scissors. It's a draw!");
@@ -54,11 +50,6 @@ if ( win <5 && lose<5){
         else {
             const computerSelection = "rock";
             console.log ("computer: " + computerSelection);
-            //let div = document.createElement('div');
-            //div.id = 'computerSelection';
-            //div.innerHTML = '<h3>The Computer chose: Rock</h3>';
-            //document.body.appendChild(div);
-
 
             if (userSelection == "rock") {
                 console.log("both of you picked rock. It's a draw!");
@@ -72,16 +63,12 @@ if ( win <5 && lose<5){
             }
         }
 
-    } else {
-        console.log("invalid input. Please try again.");
-    }
+  
     console.log("Draw: " + draw + ". Win: " + win + ". Lose: " + lose + ".");   
-    //const computerSelect = document.getElementById("computerSelect");
     const  winScore = document.getElementById("winScore");
     const loseScore = document.getElementById("loseScore");
     const drawScore = document.getElementById("drawScore");
 
-    //computerSelect.textContent = `Computer selected : ${computerSelect}`;
     winScore.textContent = `Player score = ${win}`;
     loseScore.textContent = `Computer score = ${lose}`;
     drawScore.textContent = `Draw = ${draw}`;
